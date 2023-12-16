f470617656b498b3028f64cea2be64256bac261f
Vaibhav <vaibhavujagare77@gmail.com>
2023-12-16 14:15:16 +0530

    add setup.py and requirements.txt

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages,setup
+from typing import List
+
+
+def get_requirements(file_path:str)->[str]:
+    requirements = []
+    with open(file_path) as f:
+        requirements = f.readlines()
+        requirements = [req.replace("\n","") for req in requirements]
+
+        if "-e ." in requirements:
+            requirements.remove("-e .")
+
+    return requirements
+
+
+
+setup(
+    name="mlproject",
+    version = "0.0.1",
+    author = "Vaibhav",
+    author_email="vaibhavujagare77@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+
+
+
+)
