1d06731d486ac7b030f582c51eb8e9ab2b38f9ce
Vaibhav <vaibhavujagare77@gmail.com>
2024-05-12 20:49:50 +0530

    First Setup

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages,setup
+from typing import List
+
+HYPEN_E_DOT = "-e ."
+
+def get_requiements(file_path:str)->List[str]:
+    """
+    this function will return the list of requirements
+    """
+    requirements = []
+    with open(file_path) as f:
+        requirements= f.readlines()
+        requirements = [req.replace('\n',"") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+
+
+setup(
+    name="MLPROJECT",
+    version='0.0.1',
+    author='vaibhav',
+    author_email='vaibhavujagare77@gmail.com',
+    packages=find_packages(),
+    install_requires = get_requiements('requirements.txt')
+
+)
